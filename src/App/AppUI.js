46aa944308ba7f5bd61054cd595860b0e789616b
yovercc <ycastroc@uni.pe>
2024-09-02 22:26:48 -0500

    Estados de carga y error

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -5,40 +5,46 @@ import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
 function AppUI({
-    completedTodos,
-    totalTodos,
-    searchValue,
-    setSearchValue,
-    filteredTodos,
-    completeTodo,
-    deleteTodo,
+  loading,
+  error,
+  completedTodos,
+  totalTodos,
+  searchValue,
+  setSearchValue,
+  filteredTodos,
+  completeTodo,
+  deleteTodo,
 }) {
-    return (
-        <>
-          <TodoCounter 
-            completed={completedTodos} 
-            total={totalTodos}
+  return (
+    <>
+      <TodoCounter 
+        completed={completedTodos} 
+        total={totalTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+
+      <TodoList>
+        {loading && <p>Estamos cargando ...</p>}
+        {error && <p>Desespérate, huno un error!!</p>}
+        {(!loading && filteredTodos.length === 0) && <p>Crea tu primer TODO!</p>}
+        
+        {filteredTodos.map(todo => (
+          <TodoItem  
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text, todo.completed)}
+            onDelete={() => deleteTodo(todo.text)}
           />
-          <TodoSearch
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-          />
-    
-          <TodoList>
-            {filteredTodos.map(todo => (
-              <TodoItem  
-                key={todo.text} 
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text, todo.completed)}
-                onDelete={() => deleteTodo(todo.text)}
-              />
-            ))}
-          </TodoList>
-    
-          <CreateTodoButton />
-        </>
-    );
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </>
+  );
 }
 
 export { AppUI };
